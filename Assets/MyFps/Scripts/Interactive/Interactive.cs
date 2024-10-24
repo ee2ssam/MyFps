31a28ebbe623717494dcfb05a8274f55b57e758f
DESKTOP-52351T8\Admin <ee2ssam@gmail.com>
2024-10-24 14:23:47 +0900

    1. 부서지는 오브젝트 구현 2. 퍼즐 아이템 획득 구현 3. 문 열기 실패/성공 구현

--- Assets/MyFps/Scripts/Interactive/Interactive.cs
+++ Assets/MyFps/Scripts/Interactive/Interactive.cs
@@ -17,7 +17,8 @@ namespace MyFps
         [SerializeField] private string action = "Action Text";
         public GameObject extraCross;
 
-        //Action
+        //true이면 Interactive 기능을 정지
+        protected bool unInteractive = false;
         #endregion
 
         private void Update()
@@ -28,7 +29,7 @@ namespace MyFps
         private void OnMouseOver()
         {
             //거리가 2이하 일때
-            if (theDistance <= 2f)
+            if (theDistance <= 2f && !unInteractive)
             {
                 ShowActionUI();
 
--- Assets/MyFps/Scripts/Player/PlayerStats.cs
+++ Assets/MyFps/Scripts/Player/PlayerStats.cs
@@ -2,23 +2,35 @@ using UnityEngine;
 
 namespace MyFps
 {
+    //퍼즐 아이템 획득 여부
+    public enum PuzzleKey
+    {
+        ROOM01_KEY,
+        MAX_KEY             //퍼즐 아이템 갯수
+    }
+
     //플레이어의 속성, 데이터값을 관리하는 (싱글톤, DontDestory)클래스.. ammoCount
     public class PlayerStats : PersistentSingleton<PlayerStats>
     {
         #region Variables
         //탄환 갯수
         [SerializeField] private int ammoCount;
+
         public int AmmoCount
         { 
             get { return ammoCount; }
             private set { ammoCount = value; }
         }
+
+        //게임 퍼즐 아이템 키
+        private bool[] puzzleKeys;
         #endregion
 
         private void Start()
         {
             //속성값/Data 초기화
             AmmoCount = 0;
+            puzzleKeys = new bool[(int)PuzzleKey.MAX_KEY];
         }
 
         public void AddAmmo(int amount)
@@ -39,6 +51,17 @@ namespace MyFps
             return true;
         }
 
+        //퍼즐 아이템 획득
+        public void AcquirePuzzleItem(PuzzleKey key)
+        {
+            puzzleKeys[(int)key] = true;
+        }
+
+        //퍼즐 아이템을 소지 여부
+        public bool HasPuzzleItem(PuzzleKey key)
+        {
+            return puzzleKeys[(int)key];
+        }
 
     }
 }
