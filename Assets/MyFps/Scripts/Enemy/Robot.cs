d280c59b7c381097adaf8e16416c7b0538e90738
ee2ssam <97661118+ee2ssam@users.noreply.github.com>
2025-05-23 15:03:42 +0900

    1. 슈팅 시스템 2. 플레이어, 로봇 스크립트 리펙토링

--- Assets/MyFps/Scripts/Enemy/Robot.cs
+++ Assets/MyFps/Scripts/Enemy/Robot.cs
@@ -11,7 +11,7 @@ namespace MyFps
         R_Death
     }
 
-    //enemy(로봇)을 제어하는 클래스
+    //enemy(로봇)의 상태 제어하는 클래스
     public class Robot : MonoBehaviour
     {
         #region Variables
@@ -19,17 +19,12 @@ namespace MyFps
         private Animator animator;
         public Transform thePlayer; //타겟
 
+        private RobotHealth robotHealth;
+
         //로봇의 현재 상태
         private RobotState robotState;
         //로봇의 이전 상태
         private RobotState beforeState;
-
-        //체력
-        private float currentHealth;
-        [SerializeField]
-        private float maxHealth = 20;
-
-        private bool isDeath = false;
 
         //이동
         [SerializeField]
@@ -53,23 +48,33 @@ namespace MyFps
         #endregion
 
         #region Unity Event Method
-        private void Start()
+        private void Awake()
         {
             //참조
             animator = this.GetComponent<Animator>();
-
-            //초기화
-            currentHealth = maxHealth;
+            robotHealth = this.GetComponent<RobotHealth>();
         }
 
         private void OnEnable()
         {
+            //이벤트 함수 등록
+            robotHealth.OnDie += OnDie;
+
             //초기화
             ChangeState(RobotState.R_Idle);
         }
 
+        private void OnDisable()
+        {
+            //이벤트 함수 해제
+            robotHealth.OnDie -= OnDie;
+        }
+
         private void Update()
         {
+            if (robotHealth.IsDeath)
+                return;
+
             //이동
             Vector3 target = new Vector3(thePlayer.position.x, 0f, thePlayer.position.z);
             Vector3 dir = target - this.transform.position;
@@ -124,31 +129,6 @@ namespace MyFps
             animator.SetInteger(enemyState, (int)robotState);
         }
 
-        //데미지 입기
-        public void TakeDamage(float damage)
-        {
-            currentHealth -= damage;
-            Debug.Log($"robot currentHealth: {currentHealth}");
-
-            //데미지 연출 (Sfx, Vfx)
-
-            if (currentHealth <= 0f && isDeath == false)
-            {
-                Die();
-            }
-        }
-
-        //죽기
-        private void Die()
-        {
-            isDeath = true;
-
-            //죽음 처리
-            ChangeState(RobotState.R_Death);
-
-            //보상처리..
-        }
-
         //2초마다 데미지를 5씩 준다
         private void OnAttackTimer()
         {
@@ -166,11 +146,18 @@ namespace MyFps
         public void Attack()
         {
             Debug.Log($"플레이어에게 {attackDamage}를 준다");
-            PlayerController playerController = thePlayer.GetComponent<PlayerController>();
-            if (playerController)
+            IDamageable damageable = thePlayer.GetComponent<IDamageable>();
+            if (damageable != null)
             {
-                playerController.TakeDamage(attackDamage);
+                damageable.TakeDamage(attackDamage);
             }
+        }
+
+        //죽음시 호출되는 함수
+        private void OnDie()
+        {
+            ChangeState(RobotState.R_Death);
+            this.GetComponent<BoxCollider>().enabled = false;
         }
         #endregion
     }
