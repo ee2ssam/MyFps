9ce4adefc96e462e3821357d73eaf10ed84af49c
ee2ssam <97661118+ee2ssam@users.noreply.github.com>
2025-05-30 15:35:10 +0900

    1. 부서지는 오브젝트 2. 퍼즐 아이템 시스템  구현

--- Assets/MyFps/Scripts/BreakableObejct.cs
+++ Assets/MyFps/Scripts/BreakableObejct.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 namespace MyFps
 {
@@ -9,17 +10,80 @@ namespace MyFps
     public class BreakableObejct : MonoBehaviour, IDamageable
     {
         #region Variables
-        public GameObject fakeObejct;
-        public GameObject realObject;
+        public GameObject fakeObejct;   //온전한 오브젝트
+        public GameObject realObject;   //부서지는 오브젝트
+        public GameObject sphereObect;  //액티브 연출용 오브젝트
 
         private bool isDeath = false;   //두번 죽는것 체크
-        private float health = 1f;        
+        private float health = 1f;
+
+        [SerializeField]
+        private bool unBreakable = false;   //깨지지 않는 오브젝트
+
+        //숨겨진 아이템
+        public GameObject hiddenItemPrefab;
+        [SerializeField]
+        private Vector3 offset;
+
+        public GameObject hiddenItem;
         #endregion
 
         #region Custom Method
         public void TakeDamage(float damage)
         {
+            //무적 모드
+            if (unBreakable)
+                return;
 
+            health -= damage;
+
+            if(health <= 0f && isDeath == false)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            isDeath = true;
+
+            //깨지는 연출
+            StartCoroutine(Break());
+        }
+
+        IEnumerator Break()
+        {
+            //충돌체 제거
+            this.GetComponent<BoxCollider>().enabled = false;
+
+            //깨지는 오브젝트 보이기
+            fakeObejct.SetActive(false);
+
+            if(sphereObect)
+            {
+                yield return new WaitForSeconds(0.1f);
+                sphereObect.SetActive(true);
+            }
+            realObject.SetActive(true);
+
+            //사운드
+            AudioManager.Instance.Play("PotterySmash");
+
+            if (sphereObect)
+            {
+                yield return new WaitForSeconds(0.1f);
+                sphereObect.SetActive(false);
+            }
+
+           /* //숨겨진 아이템 나타내기
+            if(hiddenItemPrefab)
+            {
+                Instantiate(hiddenItemPrefab, transform.position + offset, Quaternion.identity);
+            }*/
+            if(hiddenItem)
+            {
+                hiddenItem.SetActive(true);
+            }
         }
         #endregion
     }
--- Assets/MyFps/Scripts/Player/PlayerDataManager.cs
+++ Assets/MyFps/Scripts/Player/PlayerDataManager.cs
@@ -9,11 +9,20 @@ namespace MyFps
         Pistol,
     }
 
+    //퍼즐 아이템 enum값 설정
+    public enum PuzzleKey
+    {
+        ROOM01_KEY,        
+        MAX_KEY              //퍼즐 아이템 갯수
+    }
+
     //플레이어 데이터 관리 클래스 - 싱글톤(다음 씬에서 데이터 보존)
     public class PlayerDataManager : PersistanceSingleton<PlayerDataManager>
     {
         #region Variables
         private int ammoCount;
+
+        private bool[] hasKeys;         //퍼즐 아이템 소지 여부 체크
         #endregion
 
         #region Property
@@ -28,8 +37,18 @@ namespace MyFps
         private void Start()
         {
             //플레이 데이터 초기화 
+            InitPlayerData();
+        }
+
+        //플레이 데이터 초기화 
+        private void InitPlayerData()
+        {
             ammoCount = 0;
             Weapon = WeaponType.None;
+
+            //퍼즐 아이템 설정: 퍼즐 아이템 갯수만큼 불형 변수 요소수 생성
+            hasKeys = new bool[(int)PuzzleKey.MAX_KEY];
+
         }
         #endregion
 
@@ -52,6 +71,18 @@ namespace MyFps
 
             ammoCount -= amount;
             return true;
+        }
+
+        //퍼즐 아이템 획득 - 매개변수로 퍼즐키 타입 받는다
+        public void GainPuzzleKey(PuzzleKey key)
+        {
+            hasKeys[(int)key] = true;
+        }
+
+        //퍼즐 아이템 소지 여부 체크 - 매개변수로 퍼즐키 타입 받는다
+        public bool HasPuzzleKey(PuzzleKey key)
+        {
+            return hasKeys[(int)key];
         }
         #endregion
 
