6ffd7527388a93c6e9d4c472339efd92a5869e43
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-11-18 16:12:52 +0900

    1. 플레이어 캐스팅 구현

--- Assets/MyFps/Scripts/DoorCellOpen.cs
+++ Assets/MyFps/Scripts/DoorCellOpen.cs
@@ -17,22 +17,60 @@ namespace MyFps
 
         [SerializeField]
         private string action = "Open The Door";
+
+        //액션
+        public Animator animator;
+        private BoxCollider collider;
+
+        //애니메이터 파라미터
+        const string Open = "Open";
         #endregion
 
         #region Unity Event Method
+        private void Awake()
+        {
+            //참조
+            collider = GetComponent<BoxCollider>();
+        }
+
         private void OnMouseOver()
         {
+            if(PlayerCasting.distanceFromTarget > 2f)
+            {
+                actionUI.SetActive(false);
+                actionText.text = "";
+                return;
+            }
+
             actionUI.SetActive(true);
             actionText.text = action;
 
             //만약 Action 버튼을 누르면
-
+            if(Input.GetButtonDown("Action"))
+            {
+                OpenDoor();
+            }
         }
 
         private void OnMouseExit()
         {
             actionUI.SetActive(false);
             actionText.text = "";
+        }
+        #endregion
+
+        #region Custom Method
+        void OpenDoor()
+        {
+            //UI
+            actionUI.SetActive(false);
+            actionText.text = "";
+
+            //애니메이션
+            animator.SetTrigger(Open);
+
+            //충돌체 기능 제거
+            collider.enabled = false;
         }
         #endregion
     }
