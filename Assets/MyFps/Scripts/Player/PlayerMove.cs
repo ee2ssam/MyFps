f47716dcb5198356d92314f73818f11e8b66cbf6
ee2ssam <ee2ssam@gmail.com>
2026-06-23 18:07:50 +0900

    1. 캐릭터 컨트롤러 1

---
+++ Assets/MyFps/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+namespace MyFps
+{
+    /// <summary>
+    /// 플레이어의 이동을 관리하는 클래스
+    /// </summary>
+    public class PlayerMove : MonoBehaviour
+    {
+        #region Variables
+        //참조
+        private CharacterController controller;
+        private CharacterInput input;
+
+        [Header ("Player")]     //헤더 특성 : 직열화된 속성중에 Player와 관련된 내용이다 표시
+        //이동
+        [SerializeField] private float walkSpeed = 4f;      //걷는 속도
+        [SerializeField] private float sprintSpeed = 7f;    //뛰는 속도
+        private float moveSpeed;                            //이동 속도
+
+        //
+        #endregion
+
+        #region Unity Event Method
+        private void Awake()
+        {
+            //참조
+            controller = GetComponent<CharacterController>();
+            input = GetComponent<CharacterInput>();
+        }
+
+        private void Update()
+        {
+            //이동
+            Move();
+        }
+        #endregion
+
+        #region Custom Method
+        void Move()
+        {
+            moveSpeed = walkSpeed;
+
+            //이동 인풋 체크
+            if (input.Move == Vector2.zero)
+                moveSpeed = 0f;
+
+            //방향
+
+
+            //이동 : 방향 * Time.deltatime * speed
+        }
+        #endregion
+
+    }
+}
