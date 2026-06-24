c039d0298f4c9dc44b5d88ad521a9576ac2e3296
ee2ssam <ee2ssam@gmail.com>
2026-06-24 15:02:34 +0900

    1. 캐릭터 애니메이션 샘플 예제

---
+++ Assets/_Sample/02CharacterAnimTest/CharacterAnimTest.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MySample
+{
+    /// <summary>
+    /// 캐릭터 애니메이션을 제어하는 예제 클래스
+    /// 뉴 인풋시스템
+    /// 기본이 대기 상태
+    /// W키가 들어오면 걷기 상태
+    /// + Shift 키를 누르면 뛰기 상태
+    /// </summary>
+    public class CharacterAnimTest : MonoBehaviour
+    {
+        #region Variables
+        //참조
+        private Animator animator;
+
+        [SerializeField] private bool isMove;
+        [SerializeField] private bool isRun;
+
+        //애니 파라미터 스트링
+        private string isMoving = "IsMove";
+        private string isRunning = "IsRun";
+
+        //인풋 액션
+        public InputActionReference moveAction;
+        public InputActionReference sprintAction;
+        #endregion
+
+        #region Property
+        public bool IsMove
+        {
+            get { return isMove; }
+            private set
+            {
+                isMove = value;
+                animator.SetBool(isMoving, value);
+            }
+        }
+
+        public bool IsRun
+        {
+            get { return isRun; }
+            private set
+            {
+                isRun = value;
+                animator.SetBool(isRunning, value);
+            }
+        }
+        #endregion
+
+        #region Unity Event Method 
+        //참조
+        private void Awake()
+        {
+            //참조
+            animator = GetComponent<Animator>();
+        }
+
+        private void OnEnable()
+        {
+            //인풋 액션 활성화
+            moveAction.action.Enable();
+            sprintAction.action.Enable();
+        }
+
+        private void OnDisable()
+        {
+            //인풋 액션 비활성화
+            moveAction.action.Disable(); 
+            sprintAction.action.Disable();
+        }
+
+        private void Update()
+        {
+            //인풋 처리
+            Vector2 inputMove = moveAction.action.ReadValue<Vector2>();
+            IsMove = inputMove != Vector2.zero;
+
+            if(sprintAction.action.WasPressedThisFrame())
+            {
+                IsRun = true;
+            }
+            else if (sprintAction.action.WasReleasedThisFrame())
+            {
+                IsRun = false;
+            }
+        }
+        #endregion
+    }
+}
