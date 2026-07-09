c9d09d05b7cd3f334749cf1f2a1a3265f9982fb4
ee2ssam <97661118+ee2ssam@users.noreply.github.com>
2026-07-09 13:25:36 +0900

    1. 셰이더 그라프 예제
    1. 셰이더 그라프 예제

--- Assets/MyFps/Scripts/Door/DoorSwitch.cs
+++ Assets/MyFps/Scripts/Door/DoorSwitch.cs
@@ -11,9 +11,33 @@ namespace MyFps
     {
         #region Variables
         public Door door;   //문 등록
+
+        public Renderer renderer;
+        public Material closeMaterial;
+        private Material originMaterial;
         #endregion
 
         #region Unity Event Method
+        private void OnEnable()
+        {
+            //문 개폐 이벤트에 함수 등록
+            door.OnActivate += OpenDoor;
+            door.OnDeActivate += CloseDoor;
+        }
+
+        private void OnDisable()
+        {
+            //문 개폐 이벤트에 함수 제거
+            door.OnActivate -= OpenDoor;
+            door.OnDeActivate -= CloseDoor;
+        }
+
+
+        private void Start()
+        {
+            //초기화
+            originMaterial = renderer.material;
+        }
         #endregion
 
         //추상메서드 - 구현하도록 강제하는 기능 정의
@@ -36,18 +60,28 @@ namespace MyFps
             if(door.IsActive)
             {
                 door.DeActivate();
-                //actionText.text = "Open The Door";
             }
             else
             {
                 door.Activate();
-                //actionText.text = "Close The Door";
             }
 
             yield return new WaitForSeconds(1f);
 
             //콜라이더 기능 복원
             this.GetComponent<BoxCollider>().enabled = true;
+        }
+
+        void OpenDoor()
+        {
+            action = "Close The Door";
+            renderer.material = closeMaterial;
+        }
+
+        void CloseDoor()
+        {
+            action = "Open The Door";
+            renderer.material = originMaterial;
         }
         #endregion
     }
--- Assets/MyFps/Scripts/PlayerData/PlayerStats.cs
+++ Assets/MyFps/Scripts/PlayerData/PlayerStats.cs
@@ -3,12 +3,29 @@ using UnityEngine;
 namespace MyFps
 {
     /// <summary>
+    /// 퍼즐 아이템 목록 정의
+    /// </summary>
+    public enum PuzzleItem
+    {
+        None = 0,
+        Door01_Key,
+        Door02_Key,
+        Left_Eye,
+        Right_Eye,
+
+        MaxPuzzleItem
+    }
+
+    /// <summary>
     /// 플레이어의 속성값들을 관리하는 싱글톤 클래스
     /// </summary>
     public class PlayerStats : PersistanctSingleton<PlayerStats>
     {
         #region Varibles
         private int ammoCount;
+
+        //퍼즐 아이템 획득 여부
+        [SerializeField] private bool[] puzzleItems;
         #endregion
 
         #region Property
@@ -20,6 +37,7 @@ namespace MyFps
         {
             //초기화
             ammoCount = 0;
+            puzzleItems = new bool[(int)PuzzleItem.MaxPuzzleItem];            
         }
         #endregion
 
@@ -40,6 +58,33 @@ namespace MyFps
             }
 
             ammoCount -= amount;
+            return true;
+        }
+
+        //퍼즐 아이템 획득 여부 체크
+        public bool HavePuzzleItem(PuzzleItem item)
+        {
+            //아이템 체크
+            if(item == PuzzleItem.None || item == PuzzleItem.MaxPuzzleItem)
+            {
+                Debug.Log("Out of Range");
+                return false;
+            }
+
+            return puzzleItems[(int)item];
+        }
+
+        //퍼즐 아이템 획득 - 획득성공 true, 실패 false
+        public bool GainPuzzleItem(PuzzleItem item)
+        {
+            //아이템 체크
+            if (item == PuzzleItem.None || item == PuzzleItem.MaxPuzzleItem)
+            {
+                Debug.Log("Out of Range");
+                return false;
+            }
+
+            puzzleItems[(int)item] = true;
             return true;
         }
         #endregion
